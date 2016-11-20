f1565c3a522a1873dc3351239db7f00f899724d9
Erol Uysal <eutkuuysal@gmail.com>
2016-11-20 14:03:49 +0300

    fixed

--- tests/builder/editorconfig.py
+++ tests/builder/editorconfig.py
@@ -4,21 +4,18 @@ from configbuilder.builder.editorconfig import EditorConfigBuilder
 
 
 class EditorConfigTest(unittest.TestCase):
-    def setUp(self):
-        self.editorconfig = EditorConfigBuilder
-
     def test_without_section(self):
-        value = self.editorconfig(None, root='true')
+        value = EditorConfigBuilder(None, root='true')
         self.assertEqual(value.__str__(), 'root = true\n')
 
     def test_with_section(self):
-        value = self.editorconfig('*', charset='utf-8')
+        value = EditorConfigBuilder('*', charset='utf-8')
         self.assertEqual(value.__str__(), '[*]\ncharset = utf-8\n')
 
     def test_with_invalid_command(self):
         with self.assertRaises(AttributeError):
-            self.editorconfig('*', foo=True)
+            EditorConfigBuilder('*', foo=True)
 
     def test_with_invalid_value(self):
         with self.assertRaises(AttributeError):
-            self.editorconfig('*', charset='foo')
+            EditorConfigBuilder('*', charset='foo')
